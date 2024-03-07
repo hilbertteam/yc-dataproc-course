61831bf4083572030ae6105361d908f37959b29e
Alexey Koloskov <alexey.koloskov@hilbertteam.com>
2024-03-07 11:38:32 +0300

    Fix infra/2.2-get-master-ssh.sh

--- infra/2.2-get-master-ssh.sh
+++ infra/2.2-get-master-ssh.sh
@@ -9,8 +9,10 @@ source `dirname "$(realpath $0)"`/2.2-dataproc-config.env
 # Получаем fqdn master-ноды кластера DataProc
 export DATAPROC_MASTER_NODE_FQDN=$(yc dataproc cluster list-hosts --name ${DATAPROC_CLUSTER_NAME} --format json | jq -r '.[] | select(.role == "MASTERNODE") | .name')
 
-# Добавляем fingerprint хоста в ~/
-ssh-keygen -R $DATAPROC_MASTER_NODE_FQDN >/dev/null
+# Добавляем fingerprint хоста в ~/.ssh/known_hosts
+if [ -f ~/.ssh/known_hosts ]; then
+  ssh-keygen -R $DATAPROC_MASTER_NODE_FQDN >/dev/null
+fi
 ssh-keyscan -H $DATAPROC_MASTER_NODE_FQDN 2>/dev/null >> ~/.ssh/known_hosts
 
 # Печатаем команду для подключения к master-ноде dataproc
