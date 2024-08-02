187c82ce606755df335ed531a9b785572d3a350e
Alexey Koloskov <alexey.koloskov@hilbertteam.com>
2024-08-02 12:07:49 +0300

    Uncomment creation of ssh key in common-create.sh

--- infra/0-common-create.sh
+++ infra/0-common-create.sh
@@ -12,9 +12,9 @@ source `dirname "$(realpath $0)"`/0-common-config.env
 ###
 # ssh
 ###
-# Создаем пару ssh-ключей если не была создана ранее
-# test -f "$SSH_KEY_FILE" || ssh-keygen -t ed25519 -f $SSH_KEY_FILE -q -N ""
-# chmod 0400 $SSH_KEY_FILE
+Создаем пару ssh-ключей если не была создана ранее
+test -f "$SSH_KEY_FILE" || ssh-keygen -t ed25519 -f $SSH_KEY_FILE -q -N ""
+chmod 0400 $SSH_KEY_FILE
 
 ###
 # common infra
