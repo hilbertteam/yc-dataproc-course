d5e7f233c4572e4a16ee278eae3d2e7ea872e711
Alexey Koloskov <alexey.koloskov@hilbertteam.com>
2025-05-19 01:34:00 +0300

    Add 6.2-airflow-infra-destroy.sh

--- infra/6.2-airflow-infra-destroy.sh
+++ infra/6.2-airflow-infra-destroy.sh
@@ -4,18 +4,11 @@ set -ux
 
 # Считываем значения переменных
 source `dirname "$(realpath $0)"`/0-common-config.env
-source `dirname "$(realpath $0)"`/infra/6.2-airflow-infra-config.env
+source `dirname "$(realpath $0)"`/6.2-airflow-infra-config.env
 
 #########
 # Infra #
 #########
-
-###
-# ssh
-###
-# Создаем пару ssh-ключей если не была создана ранее
-test -f "$SSH_KEY_FILE" || ssh-keygen -t ed25519 -f $SSH_KEY_FILE -q -N ""
-chmod 0400 $SSH_KEY_FILE
 
 ###
 # airflow infra
@@ -29,6 +22,5 @@ source `dirname "$(realpath $0)"`/terraform/lessons/6.2-airflow-infra/tf.env
 cd `dirname "$(realpath $0)"`/terraform/lessons/6.2-airflow-infra
 terraform init -upgrade
 
-# План и апплай
-terraform plan
-terraform apply
+# Дестрой
+terraform destroy
